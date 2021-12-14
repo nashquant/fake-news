d0f704b306bee75b7a882bdebebc430a23ed0fd8
Matheus Augusto Campos Pires <matheus@loyall.com.br>
2021-12-14 12:36:48 -0300

    checkpoint

--- fake_news/train.py
+++ fake_news/train.py
@@ -2,12 +2,18 @@ import argparse
 import json
 import logging
 import os
+import sys
 import random
 from shutil import copy
 
 import mlflow
 import numpy as np
 import torch
+
+file_dir = os.path.dirname(__file__)
+base_dir = os.path.dirname(file_dir)
+
+sys.path.append(base_dir)
 
 from fake_news.model.transformer_based import RobertaModel
 from fake_news.model.tree_based import RandomForestModel
@@ -38,10 +44,10 @@ if __name__ == "__main__":
     args = read_args()
     with open(args.config_file) as f:
         config = json.load(f)
-    
+
     set_random_seed(42)
     mlflow.set_experiment(config["model"])
-    
+
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     model_output_path = os.path.join(base_dir, config["model_output_path"])
     # Update full model output path
@@ -55,7 +61,7 @@ if __name__ == "__main__":
         mlflow.set_tags({
             "evaluate": config["evaluate"]
         })
-        
+
         train_data_path = os.path.join(base_dir, config["train_data_path"])
         val_data_path = os.path.join(base_dir, config["val_data_path"])
         test_data_path = os.path.join(base_dir, config["test_data_path"])
@@ -63,22 +69,24 @@ if __name__ == "__main__":
         train_datapoints = read_json_data(train_data_path)
         val_datapoints = read_json_data(val_data_path)
         test_datapoints = read_json_data(test_data_path)
-        
+
         if config["model"] == "random_forest":
-            config["featurizer_output_path"] = os.path.join(base_dir, config["featurizer_output_path"])
+            config["featurizer_output_path"] = os.path.join(
+                base_dir, config["featurizer_output_path"])
             model = RandomForestModel(config)
         elif config["model"] == "roberta":
             model = RobertaModel(config)
         else:
             raise ValueError(f"Invalid model type {config['model']} provided")
-        
+
         if not config["evaluate"]:
             LOGGER.info("Training model...")
-            model.train(train_datapoints, val_datapoints, cache_featurizer=True)
+            model.train(train_datapoints, val_datapoints,
+                        cache_featurizer=True)
             if config["model"] == "random_forest":
                 # Cache model weights on disk
                 model.save(os.path.join(model_output_path, "model.pkl"))
-        
+
         mlflow.log_params(model.get_params())
         LOGGER.info("Evaluating model...")
         val_metrics = model.compute_metrics(val_datapoints, split="val")
